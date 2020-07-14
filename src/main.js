02cdec5d1aebacd88d42815d9c887dc6e8ac9464
julia <julia.ignatova11@gmail.com>
2020-07-14 19:13:35 +0300

    removing extra files

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@
  */
 import Vue from 'vue';
 import App from './App.vue';
-// You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
 import router from './router';
 import NowUiKit from './plugins/now-ui-kit';
 import LetItSnow from 'vue-let-it-snow';
